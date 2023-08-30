87c750eb0ef7eab7a244f19dc8e641b5acc9e81a
ashish@123456 <ashishkhankari0927@gmail.com>
2023-08-30 19:27:51 +0530

    Emmiter

--- emmiter.js
+++ emmiter.js
@@ -2,25 +2,63 @@ const crypto = require('crypto'); //require crypto
 const User = require('./dataSend') // get schema file
 const myData = require('./data.json')  // get json file
 const db = require('./monoose') //require mongoose file
-
-
-const algorithm = "aes-256-cbc"
-const initVector = crypto.randomBytes(16)
-const message  = "Hi There"
-const Securitykey = crypto.randomBytes(32);
-const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
-
-let encryptedData = cipher.update(message, "utf-8", "hex")
-encryptedData+= cipher.final('hex')
+const net = require('net')
+const port = 5000
 
 const names = myData.names;
 const cities = myData.cities;
 
-function generateMessage(){
-    const name = Math.floor(Math.random() * names.length)
-    const city = Math.floor(Math.random() * cities.length)
-    console.log(name)
-    console.log(city)
+function generateRandomMessage() {
+    const name = names[Math.floor(Math.random() * names.length)]
+    const city = cities[Math.floor(Math.random() * cities.length)]
+    const destination = cities[Math.floor(Math.random() * cities.length)];
+
+    const originalMessage = {
+        name: name,
+        city: city,
+        destination: destination
+    }
+    const secret_key = crypto.createHash('sha256').update(JSON.stringify(originalMessage)).digest('hex')
+
+    const sumCheckMessage = {
+        ...originalMessage,
+        secret_key: secret_key
+    }
+
+    return sumCheckMessage
 }
-console.log("Encrpted Data: " + encryptedData)
+const client = new net.Socket()
+const server = net.createServer((socket) => {
+
+    client.connect(port, 'localhost', () => {
+        setInterval(() => {
+            const algorithm = "aes-256-cbc"
+            const initVector = crypto.randomBytes(16)
+            const Securitykey = crypto.randomBytes(32);
+
+            const messages = [];
+            const numberOfMessages = Math.floor(Math.random() * (499 - 49) + 49);
+
+            for (let i = 0; i < numberOfMessages; i++) {
+                const message = generateRandomMessage();
+                messages.push(message);
+            }
+
+            const encryptedMessages = messages.map((msg) => {
+                const cipher = crypto.createCipheriv(algorithm, Securitykey, initVector);
+                let encryptedData = cipher.update(JSON.stringify(msg), "utf-8", "hex");
+                encryptedData += cipher.final('hex');
+                return encryptedData;
+            });
+
+            const messageStream = encryptedMessages.join('|');
+            client.write(messageStream);
+        }, 10000);
+    });
+})
+
+
+server.listen(port, '127.0.0.1', () => {
+    console.log(`Server listening on port ${port}`);
+});
 
